d08403820ee8ced00df9ea28533136fed443ea61
Mary <marya101204@gmail.com>
2025-02-05 12:41:39 +0200

    Добавьте файлы проекта.

---
+++ SystemProgramming/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SystemProgramming
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar1 = new ProgressBar();
+            progressBar2 = new ProgressBar();
+            progressBar3 = new ProgressBar();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(54, 47);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(459, 61);
+            progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            progressBar2.Location = new Point(54, 171);
+            progressBar2.Name = "progressBar2";
+            progressBar2.Size = new Size(459, 61);
+            progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            progressBar3.Location = new Point(54, 293);
+            progressBar3.Name = "progressBar3";
+            progressBar3.Size = new Size(459, 61);
+            progressBar3.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(54, 124);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(137, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Запустить 1-й поток";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(360, 124);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(167, 19);
+            checkBox2.TabIndex = 4;
+            checkBox2.Text = "Приостановить 1-й поток";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(54, 249);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(137, 19);
+            checkBox3.TabIndex = 5;
+            checkBox3.Text = "Запустить 2-й поток";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(360, 249);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(167, 19);
+            checkBox4.TabIndex = 6;
+            checkBox4.Text = "Приостановить 2-й поток";
+            checkBox4.UseVisualStyleBackColor = true;
+            checkBox4.CheckedChanged += checkBox4_CheckedChanged;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(54, 378);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(137, 19);
+            checkBox5.TabIndex = 7;
+            checkBox5.Text = "Запустить 3-й поток";
+            checkBox5.UseVisualStyleBackColor = true;
+            checkBox5.CheckedChanged += checkBox5_CheckedChanged;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(360, 378);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(167, 19);
+            checkBox6.TabIndex = 8;
+            checkBox6.Text = "Приостановить 3-й поток";
+            checkBox6.UseVisualStyleBackColor = true;
+            checkBox6.CheckedChanged += checkBox6_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(569, 450);
+            Controls.Add(checkBox6);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(progressBar3);
+            Controls.Add(progressBar2);
+            Controls.Add(progressBar1);
+            Name = "Form1";
+            Text = "Работа с потоками";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private ProgressBar progressBar2;
+        private ProgressBar progressBar3;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+    }
+}
---
+++ SystemProgramming/Form1.cs
@@ -0,0 +1,271 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+
+namespace SystemProgramming
+{
+    public partial class Form1 : Form
+    {
+        ManualResetEvent event_for_suspend1 = new ManualResetEvent(true);//управляет паузой в потоке (true означает, что поток может работать)
+        ManualResetEvent event_for_stop1 = new ManualResetEvent(false);// сигнализирует о завершении (false - поток работает, true - нужно остановить)
+        ManualResetEvent event_for_suspend2 = new ManualResetEvent(true);//управляет паузой в потоке (true означает, что поток может работать)
+        ManualResetEvent event_for_stop2 = new ManualResetEvent(false);// сигнализирует о завершении (false - поток работает, true - нужно остановить)
+        ManualResetEvent event_for_suspend3 = new ManualResetEvent(true);//управляет паузой в потоке (true означает, что поток может работать)
+        ManualResetEvent event_for_stop3 = new ManualResetEvent(false);// сигнализирует о завершении (false - поток работает, true - нужно остановить)
+
+        public SynchronizationContext uiContext;//используется для взаимодействия потока с UI (чтобы обновлять интерфейс из потока)
+        public Form1()
+        {
+            InitializeComponent();
+            // Получим контекст синхронизации для текущего потока 
+            uiContext = SynchronizationContext.Current;
+        }
+        private void ThreadFunk1()
+        {
+            try
+            {
+                uiContext.Send(d => progressBar1.Minimum = 0, null);
+                uiContext.Send(d => progressBar1.Maximum = (int)d, 230);
+                uiContext.Send(d => progressBar1.Value = 0, null);
+
+                while (!event_for_stop1.WaitOne(0)) //будет выполняться, пока не будет установлен сигнал для остановки
+                {
+                    event_for_suspend1.WaitOne(); //Проверяем, нужно ли приостановить выполнение потока
+
+                    for (int i = 0; i < 230; i++)
+                    {
+                        Thread.Sleep(50); 
+
+                        if (event_for_stop1.WaitOne(0)) // Проверяем, не был ли дан сигнал о завершении работы потока
+                            break;
+
+                        // uiContext.Send отправляет синхронное сообщение в контекст синхронизации
+                        // SendOrPostCallback - делегат указывает метод, вызываемый при отправке сообщения в контекст синхронизации. 
+                        uiContext.Send(d => progressBar1.Value = (int)d /* Вызываемый делегат SendOrPostCallback */,
+                            i /* Объект, переданный делегату */); // добавляем в список имя клиента
+
+                        event_for_suspend1.WaitOne(); //Проверяем, нужно ли приостановить выполнение потока
+                    }
+                }
+
+                uiContext.Send(d => progressBar1.Value = 0, null); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+
+        private void ThreadFunk2()
+        {
+
+            try
+            {
+                uiContext.Send(d => progressBar2.Minimum = 0, null);
+                uiContext.Send(d => progressBar2.Maximum = (int)d, 230);
+                uiContext.Send(d => progressBar2.Value = 0, null);
+
+                while (!event_for_stop2.WaitOne(0))
+                {
+                    event_for_suspend2.WaitOne();
+
+                    for (int i = 0; i < 230; i++)
+                    {
+                        Thread.Sleep(50);
+
+                        if (event_for_stop2.WaitOne(0))
+                            break;
+
+                        // uiContext.Send отправляет синхронное сообщение в контекст синхронизации
+                        // SendOrPostCallback - делегат указывает метод, вызываемый при отправке сообщения в контекст синхронизации. 
+                        uiContext.Send(d => progressBar2.Value = (int)d /* Вызываемый делегат SendOrPostCallback */,
+                            i /* Объект, переданный делегату */); // добавляем в список имя клиента
+
+                        event_for_suspend2.WaitOne();
+                    }
+                }
+
+                uiContext.Send(d => progressBar2.Value = 0, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void ThreadFunk3()
+        {
+
+            try
+            {
+                uiContext.Send(d => progressBar3.Minimum = 0, null);
+                uiContext.Send(d => progressBar3.Maximum = (int)d, 230);
+                uiContext.Send(d => progressBar3.Value = 0, null);
+
+                while (!event_for_stop3.WaitOne(0))
+                {
+                    event_for_suspend3.WaitOne();
+
+                    for (int i = 0; i < 230; i++)
+                    {
+                        Thread.Sleep(50);
+
+                        if (event_for_stop3.WaitOne(0))
+                            break;
+
+                        // uiContext.Send отправляет синхронное сообщение в контекст синхронизации
+                        // SendOrPostCallback - делегат указывает метод, вызываемый при отправке сообщения в контекст синхронизации. 
+                        uiContext.Send(d => progressBar3.Value = (int)d /* Вызываемый делегат SendOrPostCallback */,
+                            i /* Объект, переданный делегату */); // добавляем в список имя клиента
+
+                        event_for_suspend3.WaitOne();
+                    }
+                }
+
+                uiContext.Send(d => progressBar3.Value = 0, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+          
+       
+            if (checkBox1.Checked)
+            {
+                checkBox1.Text = "Остановить 1-й поток";
+                event_for_stop1.Reset();
+                // Создание делегата функции, в которой будет работать новый поток
+                ThreadStart MethodThread = new ThreadStart(ThreadFunk1);
+                // Создание объекта потока
+                Thread th1 = new Thread(MethodThread);
+                th1.IsBackground = true;
+                // Старт потока
+                th1.Start();
+            
+
+            }
+            else
+            {
+                checkBox1.Text = "Запустить 1-й поток";
+                event_for_stop1.Set();
+
+            }
+        }
+
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+         
+            if (checkBox3.Checked)
+            {
+                checkBox3.Text = "Остановить 2-й поток";
+                event_for_stop2.Reset();
+                // Создание делегата функции, в которой будет работать новый поток
+                ThreadStart MethodThread = new ThreadStart(ThreadFunk2);
+                // Создание объекта потока
+                Thread th2 = new Thread(MethodThread);
+                th2.IsBackground = true;
+                // Старт потока
+                th2.Start();
+
+            }
+            else
+            {
+                checkBox3.Text = "Запустить 2-й поток";
+                event_for_stop2.Set();
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+    
+
+            if (checkBox5.Checked)
+            {
+                checkBox5.Text = "Остановить 3-й поток";
+                event_for_stop3.Reset();
+                // Создание делегата функции, в которой будет работать новый поток
+                ThreadStart MethodThread = new ThreadStart(ThreadFunk3);
+                // Создание объекта потока
+                Thread th3 = new Thread(MethodThread);
+                th3.IsBackground = true;
+                // Старт потока
+                th3.Start();
+               
+
+            }
+            else
+            {
+                checkBox5.Text = "Запустить 3-й поток";
+                event_for_stop3.Set();
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+         
+            if (checkBox2.Checked)
+            {
+                checkBox2.Text = "Возобновить 1-й поток";
+                event_for_suspend1.Reset();
+            }
+            else
+            { 
+                checkBox2.Text = "Приостановить 1-й поток";
+                event_for_suspend1.Set();
+            }
+        }
+
+        
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            if (checkBox4.Checked)
+            {
+                checkBox4.Text = "Возобновить 2-й поток";
+                event_for_suspend2.Reset();
+            }
+            else 
+            {
+                checkBox4.Text = "Приостановить 2-й поток";
+                event_for_suspend2.Set();
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+          
+            if (checkBox6.Checked)
+            {
+                checkBox6.Text = "Возобновить 3-й поток";
+                event_for_suspend3.Reset();
+            }
+            else
+            {
+                checkBox6.Text = "Приостановить 3-й поток";
+                event_for_suspend3.Set();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            event_for_stop1.Set();
+            event_for_stop2.Set();
+            event_for_stop3.Set();
+        }
+
+    }
+}
