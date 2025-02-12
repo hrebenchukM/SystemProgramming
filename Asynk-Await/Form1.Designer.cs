f1ac11f4780df5828a8bf01e4809e445c790336e
Mary <marya101204@gmail.com>
2025-02-12 13:20:41 +0200

    ..

---
+++ Asynk-Await/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Asynk_Await
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            FileButtton = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            progressBar1 = new ProgressBar();
+            radioButtonEncrypt = new RadioButton();
+            radioButtonDecrypt = new RadioButton();
+            StartButton = new Button();
+            CancelButton = new Button();
+            SuspendLayout();
+            // 
+            // FileButtton
+            // 
+            FileButtton.BackColor = SystemColors.Control;
+            FileButtton.Location = new Point(27, 26);
+            FileButtton.Name = "FileButtton";
+            FileButtton.Size = new Size(87, 24);
+            FileButtton.TabIndex = 6;
+            FileButtton.Text = "Файл...";
+            FileButtton.UseVisualStyleBackColor = false;
+            FileButtton.Click += FileButtton_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(138, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(498, 23);
+            textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(46, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Пароль";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(138, 70);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(101, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(27, 115);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(609, 31);
+            progressBar1.TabIndex = 10;
+            // 
+            // radioButtonEncrypt
+            // 
+            radioButtonEncrypt.AutoSize = true;
+            radioButtonEncrypt.Location = new Point(288, 70);
+            radioButtonEncrypt.Name = "radioButtonEncrypt";
+            radioButtonEncrypt.Size = new Size(102, 19);
+            radioButtonEncrypt.TabIndex = 11;
+            radioButtonEncrypt.TabStop = true;
+            radioButtonEncrypt.Text = "Зашифровать";
+            radioButtonEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDecrypt
+            // 
+            radioButtonDecrypt.AutoSize = true;
+            radioButtonDecrypt.Location = new Point(427, 70);
+            radioButtonDecrypt.Name = "radioButtonDecrypt";
+            radioButtonDecrypt.Size = new Size(108, 19);
+            radioButtonDecrypt.TabIndex = 12;
+            radioButtonDecrypt.TabStop = true;
+            radioButtonDecrypt.Text = "Расшифровать";
+            radioButtonDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            StartButton.BackColor = SystemColors.Control;
+            StartButton.Location = new Point(427, 167);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(87, 24);
+            StartButton.TabIndex = 13;
+            StartButton.Text = "Пуск";
+            StartButton.UseVisualStyleBackColor = false;
+            StartButton.Click += StartButton_Click;
+            // 
+            // CancelButton
+            // 
+            CancelButton.BackColor = SystemColors.Control;
+            CancelButton.Location = new Point(549, 167);
+            CancelButton.Name = "CancelButton";
+            CancelButton.Size = new Size(87, 24);
+            CancelButton.TabIndex = 14;
+            CancelButton.Text = "Отмена";
+            CancelButton.UseVisualStyleBackColor = false;
+            CancelButton.Click += CancelButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(668, 209);
+            Controls.Add(CancelButton);
+            Controls.Add(StartButton);
+            Controls.Add(radioButtonDecrypt);
+            Controls.Add(radioButtonEncrypt);
+            Controls.Add(progressBar1);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(FileButtton);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button FileButtton;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private ProgressBar progressBar1;
+        private RadioButton radioButtonEncrypt;
+        private RadioButton radioButtonDecrypt;
+        private Button StartButton;
+        private Button CancelButton;
+    }
+}
---
+++ Asynk-Await/Form1.cs
@@ -0,0 +1,177 @@
+using System;
+
+namespace Asynk_Await
+{
+    public partial class Form1 : Form
+    {
+        public SynchronizationContext uiContext;//используется для взаимодействия потока с UI (чтобы обновлять интерфейс из потока)
+        CancellationTokenSource cts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Получим контекст синхронизации для текущего потока 
+            uiContext = SynchronizationContext.Current;
+
+        }
+
+        private void FileButtton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog.FileName;
+            }
+        }
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+
+            string source = textBox1.Text;
+            string key = textBox2.Text;
+            if (source == null || key == null)
+            {
+                MessageBox.Show("Источник или пароль пуст", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            if (!radioButtonEncrypt.Checked && !radioButtonDecrypt.Checked)
+            {
+                MessageBox.Show("Не выбран режим: Зашифровка или Расшифровка", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            progressBar1.Value = 0;
+
+         
+            cts = new CancellationTokenSource();
+            try
+            {
+                Task t1;
+                if (radioButtonEncrypt.Checked)
+                {
+                    // Зашифровать
+                    t1 = EncryptDecrypt(source, key, cts.Token, true);
+                }
+                else
+                {
+                    // Расшифровать
+                    t1 = EncryptDecrypt(source, key, cts.Token, false);
+                }
+
+                await t1;
+            }
+            catch (OperationCanceledException ex)
+            {
+                MessageBox.Show("Задача отменена.", "Отмена", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                progressBar1.Value = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            cts.Cancel(); // отмена асинхронной операции
+        }
+
+
+         async Task EncryptDecrypt(string source,string key, CancellationToken token,bool encdec)
+         {
+            string receiver = encdec ? source + ".xor" : source + ".dec";
+            byte[] keyBytes = System.Text.Encoding.UTF8.GetBytes(key);
+
+            FileStream source_file = null;
+            FileStream receiver_file = null;
+            BinaryReader reader = null;
+            BinaryWriter writer = null;
+
+
+            try
+            {
+                source_file = new FileStream(source, FileMode.Open, FileAccess.Read);
+                receiver_file = new FileStream(receiver, FileMode.Create, FileAccess.Write);
+                reader = new BinaryReader(source_file);
+                writer = new BinaryWriter(receiver_file);
+
+
+                byte[] buff = new byte[512];
+                long allBytes = new FileInfo(source).Length;  // общий размер файла
+                long bytesProc = 0;  // Кол-во обработтанных байт
+
+                uiContext.Send(d => progressBar1.Minimum = 0, null);
+                uiContext.Send(d => progressBar1.Maximum = 100, null);
+                uiContext.Send(d => progressBar1.Value = 0, null);
+
+                await Task.Run(async () =>
+                {
+
+
+
+                    while (bytesProc < allBytes)
+                    {
+                           
+                        token.ThrowIfCancellationRequested();
+                       
+                        int bytesRead = reader.Read(buff, 0, buff.Length);
+                        for (int i = 0; i < bytesRead; i++)
+                        {
+                            if (encdec)
+                            {
+                                // Зашифровка: XOR с ключом
+                                buff[i] ^= keyBytes[i % keyBytes.Length];
+                            }
+                            else
+                            {
+                                // Расшифровка: XOR с тем же ключом
+                                buff[i] ^= keyBytes[i % keyBytes.Length];
+                            }
+                        }
+                        writer.Write(buff, 0, bytesRead);
+                        bytesProc += bytesRead;
+
+                        // uiContext.Send отправляет синхронное сообщение в контекст синхронизации
+                        // SendOrPostCallback - делегат указывает метод, вызываемый при отправке сообщения в контекст синхронизации. 
+                        uiContext.Send(d => progressBar1.Value = (int)((bytesProc * 100) / allBytes) /* Вызываемый делегат SendOrPostCallback */, null);
+                        await Task.Delay(2000);
+                    }
+
+                }, token);
+                MessageBox.Show("Файл успешно обработан!", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (OperationCanceledException ex)
+            {
+                if (File.Exists(receiver))
+                {
+                    File.Delete(receiver);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+
+                writer.Close();
+                reader.Close();
+                source_file.Close();
+                receiver_file.Close();
+            }
+
+
+         }
+
+
+    }
+}
